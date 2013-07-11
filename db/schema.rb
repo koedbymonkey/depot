be2c60da339e64378dafa1eae38e37687ef60264
koedbymonkey <tsnavely4@gmail.com>
2013-07-10 19:27:22 -0700

    Fixed "Add to cart" button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710105044) do
+ActiveRecord::Schema.define(:version => 20130710120604) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
