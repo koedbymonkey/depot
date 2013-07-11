071d6676ad8e64fabfe0251e505027497da1ed6d
koedbymonkey <tsnavely4@gmail.com>
2013-07-10 19:03:40 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '0d7a7230f7767ecb52bdf7785198b9d911f48b5d2aaf53a1f605763faa7dced153c582a69fe23b3c8cc3187d98d4ed2987e42cd41338576d87098569340d7514'
---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.delete_all
+
+Product.create(title: 'Programming Ruby 1.9',
+description:%{<p>Ruby is cool.</p>},
+image_url: 'ruby.jpg',
+price: 49.95)
